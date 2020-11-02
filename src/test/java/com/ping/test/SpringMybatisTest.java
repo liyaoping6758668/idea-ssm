3b02089259602bde4869fbef5d9b5b85219094c2
IT教程资源 <3092994143@qq.com>
2020-11-02 22:47:07 +0800

    初始化提交ssm代码

---
+++ src/test/java/com/ping/test/SpringMybatisTest.java
@@ -0,0 +1,23 @@
+package com.ping.test;
+
+
+import com.ping.pojo.Item;
+import com.ping.service.ItemService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext-dao.xml","classpath:applicationContext-service.xml"})
+public class SpringMybatisTest {
+    @Autowired
+    private ItemService itemService;
+
+    @Test
+    public void test1(){
+        Item item = itemService.findById(1);
+        System.out.println(item);
+    }
+}
